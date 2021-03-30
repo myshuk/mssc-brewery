508a3876e43ea597a3643815e0636f1e2fc75097
Malay <a@b.c>
2021-03-30 14:26:40 +0100

    changing gitignore and adding spring starter project

---
+++ src/main/java/dex/neo/msscbrewery/web/model/BeerDto.java
@@ -0,0 +1,19 @@
+package dex.neo.msscbrewery.web.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BeerDto {
+    private UUID id;
+    private String name;
+    private String style;
+    private String upc;
+}
